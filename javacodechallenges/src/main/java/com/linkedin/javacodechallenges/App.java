3cd9afb6ab4ae0fbce19ccbe696bea7d778c753b
Fumiaki Yamaoka <pfhfxy0@yahoo.com>
2024-04-27 22:26:31 +0000

    Done.

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,6 +5,26 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
+        if (password.length() < 6) {
+            return false;
+        }
+        boolean hasDigit = false;
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) {
+                hasDigit = true;
+            }
+            if (Character.isUpperCase(password.charAt(i))) {
+                hasUpper = true;
+            }
+            if (Character.isLowerCase(password.charAt(i))) {
+                hasLower = true;
+            }
+            if (hasDigit && hasUpper && hasLower) {
+                return true;
+            }
+        }
         return false;
     }
 
